35131fe01cabd064c2e0e90b95b0da36818f25d2
kate <kate.reva.dev@gmail.com>
2023-07-03 15:46:53 +0300

    goods uses react-bootstrap

--- src/components/Goods.jsx
+++ src/components/Goods.jsx
@@ -1,22 +1,32 @@
-import NoImagePlaceholder from "../assets/no-image-placeholder.png"
+import { Button, Card } from "react-bootstrap";
+import Holder from "../assets/holder.svg"
 
 const Goods = ({ goods, addToCart }) => {
     return (
         <div className="goods">
             {goods.length ? goods.map((item) => {
-                return (
-                    <div key={`good-${item.id}`} className="good-item">
-                        <img className="good-item__img" src={NoImagePlaceholder} />
-                        <div className="good-item__name">{item.name}</div>
-                        <div>{item?.price || 0} <span>{item?.currency || "UAH"}</span></div>
-                        <button className="button" onClick={() => addToCart(item)}>
-                            Add to cart
-                        </button>
-                    </div>
-                );
+                return <Good item={item} addToCart={addToCart} key={item.id} />;
             }) : <span>Nothing here...</span>}
         </div>
     );
-}
+};
+
+const Good = ({ item, addToCart }) => {
+    return (
+        <Card style={{ width: '18rem', margin: '8px' }}>
+            <Card.Img variant="top" src={Holder} />
+            <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+                <Card.Text>
+                    {item.name}
+                </Card.Text>
+                <Card.Subtitle className="mb-2 text-muted">
+                    {item?.price || 0} {' '} {item?.currency || "UAH"}
+                </Card.Subtitle>
+                <Button variant="dark" onClick={() => addToCart(item)}>Add to cart</Button>
+            </Card.Body>
+        </Card>
+    );
+};
 
 export default Goods;
